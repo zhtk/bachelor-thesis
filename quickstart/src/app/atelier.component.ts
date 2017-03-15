a2cba714676714f5dcf914f98e0680e217a15baf
skazy <js347267@students.mimuw.edu.pl>
2017-03-16 00:02:40 +0100

    dodano brakujacy plik do repo

---
+++ quickstart/src/app/atelier.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atelier',
+  template: `<h2>OK!</h2>`
+  // templateUrl: 'pages/dashboard.html',
+})
+export class AtelierComponent {}
