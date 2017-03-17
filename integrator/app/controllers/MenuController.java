35bc17a57625aae06527cef2e8daa899273fcd17
Piotr Zalas <pz361374@students.mimuw.edu.pl>
2017-03-17 17:02:01 +0100

    Pobieranie początkowej końcówki

--- integrator/app/controllers/MenuController.java
+++ integrator/app/controllers/MenuController.java
@@ -1,18 +1,40 @@
 package controllers;
 
 import javax.inject.Inject;
+import com.fasterxml.jackson.databind.node.*;
+import play.libs.Json;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Results;
+import managers.CuratorHelper;
 
 public class MenuController extends Controller {
+    private CuratorHelper helper;
+    private final String START_ENDPOINT = "/start";
+    
     @Inject
-    public MenuController() {
-
+    public MenuController(CuratorHelper helper) {
+        this.helper = helper;
     }
-
+    
+    private void buildMenu(ArrayNode node, String path) {
+        
+    }
+    
+    private String getStartingEndpoint() {
+        return helper.getNodeContent(START_ENDPOINT);
+    }
+    
     public Result index() {
-        return Results.ok("OK");
+        ObjectNode result = Json.newObject();
+        
+        // Końcówka startowa
+        result.put("start", getStartingEndpoint());
+        
+        // Menu
+        ArrayNode menu = result.putArray("menu");
+        buildMenu(menu, "/");
+        
+        return Results.ok(result);
     }
 }
