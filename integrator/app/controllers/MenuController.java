53b949add8133ab11c5c963e78e63188cbe1d460
Piotr Zalas <pz361374@students.mimuw.edu.pl>
2017-03-17 19:28:03 +0100

    Konstrukcja menu

--- integrator/app/controllers/MenuController.java
+++ integrator/app/controllers/MenuController.java
@@ -1,24 +1,74 @@
 package controllers;
 
+import java.util.*;
 import javax.inject.Inject;
+import org.apache.curator.framework.recipes.cache.*;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.node.*;
 import play.libs.Json;
-import play.mvc.Controller;
-import play.mvc.Result;
-import play.mvc.Results;
+import play.mvc.*;
 import managers.CuratorHelper;
 
 public class MenuController extends Controller {
-    private CuratorHelper helper;
     private final String START_ENDPOINT = "/start";
+    private final String MENU_PATH = "/menu";
+    
+    private CuratorHelper helper;
+    private TreeCache tree;
     
     @Inject
-    public MenuController(CuratorHelper helper) {
+    public MenuController(CuratorHelper helper) throws Exception {
         this.helper = helper;
+        this.tree = new TreeCache(helper.getClient(), MENU_PATH);
+        this.tree.start();
+    }
+    
+    private void appendValue(JsonNode from, ObjectNode to, String field) {
+        List<String> list = from.findValuesAsText(field);
+        
+        if (list.isEmpty())
+            to.put(field, "");
+        else
+            to.put(field, list.get(0));
+    }
+    
+    private boolean checkPermissions(String mask) {
+        return true; // TODO
     }
     
     private void buildMenu(ArrayNode node, String path) {
+        Map<String, ChildData> items = tree.getCurrentChildren(path);
         
+        if (items == null)
+            return;
+        
+        for (Map.Entry<String, ChildData> entry : items.entrySet()) {
+            byte[] data = entry.getValue().getData();
+            JsonNode json = Json.parse(data);
+            ObjectNode child = Json.newObject();
+            
+            // Dopisanie zwykłych parametrów
+            appendValue(json, child, "nr");
+            appendValue(json, child, "name");
+            appendValue(json, child, "icon");
+            appendValue(json, child, "link");
+            
+            // Dopisanie elementów dzieci
+            ArrayNode menu = child.putArray("menu");
+            String childPath = entry.getValue().getPath();
+            buildMenu(menu, childPath);
+            
+            // Sprawdzenie uprawnień
+            String mask;
+            
+            if (!json.findValuesAsText("mask").isEmpty())
+                mask = json.findValuesAsText("mask").get(0);
+            else
+                mask = null;
+            
+            if (checkPermissions(mask))
+                node.add(child);
+        }
     }
     
     private String getStartingEndpoint() {
@@ -33,7 +83,7 @@ public class MenuController extends Controller {
         
         // Menu
         ArrayNode menu = result.putArray("menu");
-        buildMenu(menu, "/");
+        buildMenu(menu, "/menu");
         
         return Results.ok(result);
     }
