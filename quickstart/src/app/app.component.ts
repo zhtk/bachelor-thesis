0455975ae3541d62784bf73c1947d54c8478bfc0
Jarek Socha <js347267@students.mimuw.edu.pl>
2017-03-29 19:49:38 +0200

    usprawnienia komponentu wiadomości

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuService } from './menu-service';
+import { MessagesService } from './service/messages.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'pages/mockup.html',
-  providers: [MenuService]
+  providers: [MenuService, MessagesService]
 })
 export class AppComponent {}
--- quickstart/src/app/home_components/messages.component.ts
+++ quickstart/src/app/home_components/messages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MessagesService } from '../service/messages.service';
+import { Message } from '../message';
 
 @Component({
   selector: 'messages',
@@ -6,21 +8,22 @@ import { Component } from '@angular/core';
 })
 export class MessagesComponent {
 	inboxName : string;
-	message: {
-		topic: string,
-		content: string,
-		date: string,
-		from: string,
+	message_array: Message[];
+	current_message: Message;
+	SIZE_MAX = 30;
+
+	constructor(private messagesService: MessagesService) {
+		this.inboxName = "Nazwa skrzynki"
+		this.message_array = messagesService.getMessagesInbox();
+		this.current_message = null;
+		if (this.message_array.length != 0) {
+			this.current_message = this.message_array[0];
+		}
 	}
 
-	constructor() {
-		this.inboxName = "Nazwa skrzynki"
-		this.message = {
-			topic: "testowy temat",
-			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			date: "2017-01-02",
-			from: "John Doe"
-
-		}
+	abbreviate(content: string) {
+		if (content.length <= this.SIZE_MAX - 3)
+			return content
+		return content.substring(0, this.SIZE_MAX) + "...";
 	}
 }
