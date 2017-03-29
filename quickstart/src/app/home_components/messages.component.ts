81e6e25b9584f83d49d4d2fba04d075ddcc45fc2
Jarek Socha <js347267@students.mimuw.edu.pl>
2017-03-29 18:57:37 +0200

    dodano zmiany do interfejsu wiadomosci

--- quickstart/src/app/home_components/messages.component.ts
+++ quickstart/src/app/home_components/messages.component.ts
@@ -4,4 +4,23 @@ import { Component } from '@angular/core';
   selector: 'messages',
   templateUrl: 'pages/home_subpages/messages.html',
 })
-export class MessagesComponent {}
+export class MessagesComponent {
+	inboxName : string;
+	message: {
+		topic: string,
+		content: string,
+		date: string,
+		from: string,
+	}
+
+	constructor() {
+		this.inboxName = "Nazwa skrzynki"
+		this.message = {
+			topic: "testowy temat",
+			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			date: "2017-01-02",
+			from: "John Doe"
+
+		}
+	}
+}
