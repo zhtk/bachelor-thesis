cdc702acf2a20566e7dc3aeada00215559b6e47f
Jarek Socha <js347267@students.mimuw.edu.pl>
2017-04-06 22:04:06 +0200

    częsciowa refaktoryzacja kreatora

--- quickstart/src/app/FormComponents/ComponentCreator.ts
+++ quickstart/src/app/FormComponents/ComponentCreator.ts
@@ -8,42 +8,39 @@ import {MicroService} from "./MicroService";
 
 export class ComponentCreator {
 
-  static componentMapping = {
-    'TextBox': TextBox,
-    'PanelGroup' : PanelGroupComponent
-  };
+	static componentMapping = {
+		'TextBox': TextBox,
+		'PanelGroup' : PanelGroupComponent
+	};
 
-  static insertComponent(factory:ComponentFactoryResolver, target:ViewContainerRef, type: string):FEComponent {
-    // Rozwiazanie tymczasowe, z mapą z góry nie chce działać
-    var compFactory: any;
-    console.log("typ" + type);
-    if (type == "TextBox") {
-      compFactory = factory.resolveComponentFactory(TextBox);
-      const ref = target.createComponent(compFactory);
-      return <FEComponent> ref.instance;
-    }
-    else if(type == "RowComponent") {
-      compFactory = factory.resolveComponentFactory(RowComponent);
-      const ref = target.createComponent(compFactory);
-      return <FEComponent> ref.instance;
-    }
-    else if (type == "PeselComponent") {
-      compFactory = factory.resolveComponentFactory(PeselComponent);
-      const ref = target.createComponent(compFactory);
-      return <FEComponent> ref.instance;
-    }
-    else if (type == "PasswordComponent") {
-      compFactory = factory.resolveComponentFactory(PasswordComponent);
-      const ref = target.createComponent(compFactory);
-      return <FEComponent> ref.instance;
-    }
-    else
-    {
-      compFactory = factory.resolveComponentFactory(PanelGroupComponent);
-      const ref = target.createComponent(compFactory);
-      return <FEComponent> ref.instance;
-    }
-  }
-  // TODO refactor!!!
-
+	static insertComponent(factory:ComponentFactoryResolver, target:ViewContainerRef, type: string):FEComponent {
+		// Rozwiazanie tymczasowe, z mapą z góry nie chce działać
+		var compFactory: any;
+		console.log("typ" + type);
+		switch (type) {
+			case ("TextBox"):
+			compFactory = factory.resolveComponentFactory(TextBox);
+			break;
+		
+			case ("RowComponent"):
+			compFactory = factory.resolveComponentFactory(RowComponent);
+			break;
+		
+			case ("PeselComponent"):
+			compFactory = factory.resolveComponentFactory(PeselComponent);
+			break;
+		
+			case ("PasswordComponent"):
+			compFactory = factory.resolveComponentFactory(PasswordComponent);
+			break;
+		
+			default: // powinno byc explicite
+			compFactory = factory.resolveComponentFactory(PanelGroupComponent);
+			break;
+		}
+			
+		const ref = target.createComponent(compFactory);
+		return <FEComponent> ref.instance;
+	}
+	// TODO refactor!!!
 }
