43218b92b5958f78aff1df045475baaa285cbda4
Piotr Zalas <pz361374@students.mimuw.edu.pl>
2017-04-13 13:38:25 +0200

    Expressjs oraz integracja z auth i integratorem

---
+++ quickstart/app.js
@@ -0,0 +1,89 @@
+var express = require('express')
+var proxyMiddleware = require('http-proxy-middleware');
+var fallbackMiddleware = require('connect-history-api-fallback');
+var grpc = require('grpc');
+var morgan = require('morgan');
+
+var authDescriptor = grpc.load(__dirname + '/auth/auth.proto');
+var authChannel = new authDescriptor.AuthService('localhost:50051', grpc.credentials.createInsecure());
+
+function authMiddleware (req, res, next) {
+	if (isLogout) {
+        var data = {token: "admin"};
+        authChannel.invalidateToken(data, function(err, data) {
+            if (err) {
+                // Error, nie można się wylogować
+                // TODO
+            } else {
+                // Wypisujemy klientowi, że się wylogował
+                // TODO
+            }
+        });
+    }
+}
+
+var proxyOptions = {
+  target: 'http://207.154.212.228:9000',
+  changeOrigin: true,
+  ws: true,
+  pathRewrite: {
+    '^/api/' : '/'
+  }
+};
+
+var proxy = proxyMiddleware(proxyOptions);
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/login', function (req, res) {
+  if (req.query.login == null || req.query.password == null) {
+    res.status(403).end();
+    return;
+  }
+  
+  var loginData = {login: req.query.login, password: req.query.password};
+
+  authChannel.getToken(loginData, function(err, data) {
+    if (err) {
+      res.status(404).end();
+    } else {
+      var status = data.status;
+      var token = data.token;
+
+      // Wypisz wynik do response
+      if (status == 'OK') {
+        res.send(token);
+      } else {
+        // Invalid login
+        res.status(403).end();
+      }
+    }
+  });
+});
+
+app.get('/logout', function (req, res) {
+  if (req.query.token == null) {
+    res.status(403).end();
+    return;
+  }
+  
+  var data = {token: req.query.token};
+  authChannel.invalidateToken(data, function(err, data) {
+    if (err) {
+      res.status(404).end();
+    } else {
+      res.end();
+    }
+  });
+});
+
+app.use('/api', proxy);
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/src' ));
+
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000!')
+});
