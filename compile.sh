55f57f753e62246d21f22a2008c304e3785b82c8
Krzysztof Stanisławek <krzysztof.stanislawek@gemius.com>
2017-04-16 23:09:10 +0200

    compile.sh with virtualenv creation and for whole repo

--- compile.sh
+++ compile.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+python3 -m virtualenv venv
+source venv/bin/activate
+pip install -r auth/requirements.txt
+
+(
+cd auth/
 wget -O compiler.exe http://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/1.2.0/protoc-gen-grpc-java-1.2.0-linux-x86_64.exe
 chmod +x compiler.exe
-
+pwd
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. auth.proto
+pwd
 protoc --plugin=protoc-gen-grpc-java="./compiler.exe" \
        --java_out="../integrator/app" \
        --grpc-java_out="../integrator/app" \
        auth.proto
+pwd
+)
