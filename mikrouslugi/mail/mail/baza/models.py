2867838842dd46adb3aacc466197002fa14e69dc
Piotr Zalas <pz361374@students.mimuw.edu.pl>
2017-05-26 11:21:21 +0200

    Wyświetlanie wiadomości

--- mikrouslugi/mail/mail/baza/models.py
+++ mikrouslugi/mail/mail/baza/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Mail(models.Model):
+    od = models.CharField(max_length=50)
+    do = models.CharField(max_length=50)
+    data = models.CharField(max_length=11)
+    topic = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return str(self.topic)
+
+admin.site.register(Mail)
--- mikrouslugi/mail/mail/baza/views.py
+++ mikrouslugi/mail/mail/baza/views.py
@@ -1,20 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+import json
 
-# Create your views here.
-json_output = json.dumps([{
-        "id": 1,
-        "from": user_data.user_id,
-        "to": "Urząd Skarbowy nr 2 w Radomiu",
-        "date": "2017-01-01",
-        "topic": "Dzień dobry",
-        "content": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    }])
 
+@csrf_exempt
 def send(request):
-	pass
+	msg = Mail(
+		od=request.POST.dict()['from'],
+		do=request.POST.dict()['to'],
+		data=request.POST.dict()['date'],
+		topic=request.POST.dict()['topic'],
+		content=request.POST.dict()['content']
+	)
+	msg.save()
+	return HttpResponse('')
 
+@csrf_exempt
 def received(request):
-	pass
+	token = request.GET.dict()['token']
+	res = []
 
+	for i in Mail.objects.filter(do=token):
+		res.append({
+			"id": i.id,
+			"from": i.od,
+			"to": i.do,
+			"date": i.data,
+			"topic": i.topic,
+			"content": i.content,
+		})
+
+	return HttpResponse(json.dumps(res, ensure_ascii=False))
+
+@csrf_exempt
 def sent(request):
-	pass
+	token = request.GET.dict()['token']
+	res = []
+
+	for i in Mail.objects.filter(od=token):
+		res.append({
+			"id": i.id,
+			"from": i.od,
+			"to": i.do,
+			"date": i.data,
+			"topic": i.topic,
+			"content": i.content,
+		})
+	
+	return HttpResponse(json.dumps(res, ensure_ascii=False))
