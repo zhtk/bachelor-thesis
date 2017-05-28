87978f3685d223026709125375c37f6ea3f930ab
Piotr Zalas <pz361374@students.mimuw.edu.pl>
2017-05-28 20:47:26 +0200

    Wysyłanie informacji o wniosku 500+ na maila

--- mikrouslugi/500plus/plus500/wnioski/views.py
+++ mikrouslugi/500plus/plus500/wnioski/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 import json
+import requests
 
 
 @csrf_exempt
@@ -21,4 +22,30 @@ def lista(request):
 
 @csrf_exempt
 def wyslij(request):
-	return HttpResponse("{\"status\":\"OK\"}")
+	autor = Wnioskodawca(
+		imie = 'Jan',
+		nazwisko = 'Kowalski',
+		pesel = '12345678901',
+		stan_cywilny = 'Kawaler',
+		obywatelstwo = 'polskie',
+		miejscowosc = 'Warszawa',
+		kod_pocztowy = '02-097',
+		ulica = 'Banacha',
+		nr_domu = '2',
+		nr_mieszkania = '2043'
+	)
+	autor.save()
+	wniosek = Wniosek(autor=autor)
+	wniosek.save()
+	
+	msg = {
+		'from':'Usługa 500 plus',
+		'to':'user',
+		'date':'2017-07-08',
+		'topic':'Złożono wniosek 500+',
+		'content':'Gratulacje, wniosek złożony. Status wniosku dostępny jest\
+			w sekcji \"Moje wnioski\"',
+	}
+	requests.post("http://localhost:9000/write/mail-send/", data=msg)
+	
+	return HttpResponse("")
